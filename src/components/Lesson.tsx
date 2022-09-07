bc6f6a7617b72c51ff2eb16b0d215afed0704019
Rafael Souza <rafael.souza@iexfy.com>
2022-09-07 17:56:37 -0300

    class 3

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react';
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from 'react-router-dom'
 
 interface LessonProps {
   title: string;
@@ -15,12 +16,12 @@ export function Lesson(props: LessonProps) {
   const availableDateFormatted = format(props.availableAt, "EEE' * 'd ' de 'MMMM' * 'k'h'mm", { locale: ptBR });
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${props.slug}`} className='group'>
       <span className="text-gray-300">
         {availableDateFormatted}
       </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
 
           {isLessonAvailable ? (
@@ -44,6 +45,6 @@ export function Lesson(props: LessonProps) {
           {props.title}
         </strong>
       </div>
-    </a>
+    </Link>
   )
 }
